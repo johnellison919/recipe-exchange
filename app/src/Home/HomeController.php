8d9a8cc013d285d1f9029e997d46d45feaec92c7
John Ellison <johnellison919@gmail.com>
2022-06-13 00:04:23 -0500

    Adjusts the Homepage
    Moves the HomeController to a grouped Home folder and changes the home view to PHP

--- app/src/Home/HomeController.php
+++ app/src/Home/HomeController.php
@@ -1,5 +1,7 @@
 <?php
 
+	namespace Home;
+
 	use Nox\RenderEngine\Exceptions\LayoutDoesNotExist;
 	use Nox\RenderEngine\Exceptions\ParseError;
 	use Nox\RenderEngine\Exceptions\ViewFileDoesNotExist;
@@ -18,27 +20,8 @@
 		 */
 		#[Route("GET", "/")]
 		public function homeView(): string{
-			return Renderer::renderView("home.html");
-		}
-
-		/**
-		 * @throws ParseError
-		 * @throws ViewFileDoesNotExist
-		 * @throws LayoutDoesNotExist
-		 */
-		#[Route("GET", "/404")]
-		public function error404View(): string{
-			return Renderer::renderView("errors/404.html");
-		}
-
-		/**
-		 * @throws ParseError
-		 * @throws ViewFileDoesNotExist
-		 * @throws LayoutDoesNotExist
-		 */
-		#[Route("GET", "/always-404")]
-		#[NeverUsable()]
-		public function always404View(): void{
-			// Never runs
+			return Renderer::renderView(
+				viewFileName: "home.php"
+			);
 		}
 	}
