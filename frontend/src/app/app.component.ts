d8a244e2d1090e540ddf491d1f80aa3b239a7529
John Ellison <37640782+johnellison919@users.noreply.github.com>
2026-02-17 08:37:24 -0600

    feat: Adds footer

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Footer } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Footer],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
