06c45decefaa9a07d21f7a3b5125bb0db4c8a9e1
John Ellison <37640782+johnellison919@users.noreply.github.com>
2026-02-17 09:59:39 -0600

    feat: Adds header and makes global styling adjustments

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { Footer } from './components/footer/footer.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [Footer],
+  imports: [FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
