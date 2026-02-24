7dbb2d7ee8ca9019d3a40eea1f15e3f21cf0471a
John Ellison <37640782+johnellison919@users.noreply.github.com>
2026-02-24 08:47:30 -0600

    feat: Add debouncing to search input

--- frontend/src/app/shared/components/sidebar/sidebar.component.ts
+++ frontend/src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,6 +1,8 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { Router, RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { Subject, debounceTime } from 'rxjs';
 import { AuthService } from '../../../core/auth.service';
 import { RecipeService } from '../../../core/recipe.service';
 import { ProfileContextService } from '../../../core/profile-context.service';
@@ -13,17 +15,35 @@ import { slugify } from '../../utils/slugify';
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
   protected readonly authService = inject(AuthService);
   protected readonly recipeService = inject(RecipeService);
   protected readonly profileContext = inject(ProfileContextService);
   protected readonly recentlyViewed = inject(RecentlyViewedService);
   private readonly router = inject(Router);
+  private readonly destroyRef = inject(DestroyRef);
 
   protected readonly searchQuery = signal('');
+  private readonly searchInput$ = new Subject<string>();
+
+  ngOnInit(): void {
+    this.searchInput$
+      .pipe(debounceTime(300), takeUntilDestroyed(this.destroyRef))
+      .subscribe((query) => {
+        this.recipeService.setSearchQuery(query);
+        if (this.router.url !== '/') {
+          this.router.navigate(['/']);
+        }
+      });
+  }
 
   protected get isOnNewRecipePage(): boolean {
     return this.router.url === '/recipes/new';
+  }
+
+  protected onSearchInput(value: string): void {
+    this.searchQuery.set(value);
+    this.searchInput$.next(value);
   }
 
   protected search(): void {
